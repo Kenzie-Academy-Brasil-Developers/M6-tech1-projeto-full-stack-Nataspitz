3fccf18150503a21ae578f3f3cce7fa6773cd610
Nata Spitz Alves <nataspitz81@gmail.com>
2023-11-29 13:45:33 -0300

    feat: add cors

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
 import 'express-async-errors'
+import cors from "cors";
 import express ,{ Application } from "express";
 import { handleErrors } from "./errors/handleErrors";
 import { clientsRouter } from "./routers/clients.routers";
@@ -7,6 +8,7 @@ import { contactsRouter } from './routers/contacts.routers';
 import { emailContactsRouter } from './routers/emailContacts.routers';
 
 export const app: Application = express()
+app.use(cors());
 app.use(express.json())
 
 app.use("/clients", clientsRouter)
